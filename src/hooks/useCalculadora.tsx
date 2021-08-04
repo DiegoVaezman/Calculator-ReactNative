3b058feb0097818f19e93da2f6a62b502c62503c
DiegoVaezman <diego.ng90@gmail.com>
2021-08-04 20:15:53 +0200

    react native calculator

---
+++ src/hooks/useCalculadora.tsx
@@ -0,0 +1,128 @@
+import React, { useRef, useState } from 'react'
+
+enum Operadores {
+    add, 
+    subtract, 
+    multiply, 
+    divide
+};
+
+export const useCalculadora = () => {
+
+    const [secondNumber, setSecondNumber] = useState('0');
+    const [number, setNumber] = useState('0');
+    const lastOperation = useRef<Operadores>();
+
+    const clean = () => {
+        setNumber('0');
+        setSecondNumber('0');
+    };
+    const buildNumber = ( textNumber: string ) => {
+        if ( number.includes('.') &&  textNumber === '.') return;
+
+        if ( number.startsWith('0') || number.startsWith('-0') ) {
+            if ( textNumber === '.' ) {
+                setNumber( number + textNumber );
+            } else if ( textNumber === '0' && number.includes('.') ) {
+                setNumber( number + textNumber );
+            } else if ( textNumber !== '0' && !number.includes('.') ) {
+                if ( number.startsWith('-0') ) {
+                    setNumber( '-' + textNumber );
+                } else {
+                    setNumber( textNumber );
+                }
+            } else if ( textNumber === '0' && !number.includes('.') ) {
+                setNumber( number );
+            } else {
+                setNumber( number + textNumber );
+            }
+        } else {
+            setNumber( number + textNumber );
+        }
+    };
+    const positiveNegative = () => {
+        if ( number.includes('-') ) {
+            setNumber( number.replace('-', '') );
+        } else {
+            setNumber( '-' + number );
+        }
+    };
+    const btnDelete = () => {
+        if ( number === '0' ) {
+            return;
+        } else if ( 
+            number === '-0' || 
+            number.length === 1 || 
+            number.length === 2 &&  number.includes('-')  
+        ) {
+            setNumber('0');
+        } else {
+            setNumber( number.slice(0, -1) );
+        }
+    };
+    const numberUp = () => {
+        if ( number.endsWith('.') ) {
+            setSecondNumber( number.slice(0, -1) );
+        } else {
+            setSecondNumber( number );
+        }
+        setNumber('0');
+    };
+    const btnDivide = () => {
+        numberUp();
+        lastOperation.current = Operadores.divide;
+    };
+    const btnMultiply = () => {
+        numberUp();
+        lastOperation.current = Operadores.multiply;
+    };
+    const btnAdd = () => {
+        numberUp();
+        lastOperation.current = Operadores.add;
+    };
+    const btnSubtract = () => {
+        numberUp();
+        lastOperation.current = Operadores.subtract;
+    };
+    const operation = () => {
+        const num1 = Number( number );
+        const num2 = Number( secondNumber );
+
+        switch ( lastOperation.current ) {
+            case Operadores.add:
+                setNumber( `${ num1 + num2 }` );
+                break;
+
+            case Operadores.subtract:
+                setNumber( `${ num2 - num1 }` );
+                break;
+                
+            case Operadores.multiply:
+                setNumber( `${ num1 * num2 }` );
+                break;
+                
+            case Operadores.divide:
+                setNumber( `${ num2 / num1 }` );
+                break;
+            
+            case undefined:
+                break;
+        };
+        setSecondNumber('0');
+        lastOperation.current = undefined;
+    };
+
+    return ({
+        buildNumber,
+        clean,
+        positiveNegative,
+        btnDelete,
+        btnDivide,
+        btnMultiply,
+        btnAdd,
+        btnSubtract,
+        operation,
+        number,
+        secondNumber
+    });
+};
---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,78 @@
+import React, { useRef, useState } from 'react';
+import { Text, View } from 'react-native';
+import { ButtonCalc } from '../components/ButtonCalc';
+import { styles } from '../theme/appTheme';
+import { useCalculadora } from '../hooks/useCalculadora';
+
+
+export const CalculadoraScreen = () => {
+
+    const {
+        buildNumber,
+        clean,
+        positiveNegative,
+        btnDelete,
+        btnDivide,
+        btnMultiply,
+        btnAdd,
+        btnSubtract,
+        operation,
+        number,
+        secondNumber
+    } = useCalculadora()
+
+    return (
+        <View style={styles.calculatorContainer}>
+            {
+                (secondNumber !== '0') &&
+                <Text style={styles.secondNumber}>{secondNumber}</Text>
+            }
+            <Text
+                style={styles.result}
+                numberOfLines={1}
+                adjustsFontSizeToFit
+            >
+                {number}
+            </Text>
+
+            {/* Row buttons */}
+            <View style={styles.row}>
+                <ButtonCalc text="C" color="usable" action={clean} />
+                <ButtonCalc text="+/-" color="usable" action={positiveNegative} />
+                <ButtonCalc text="del" color="usable" action={btnDelete} />
+                <ButtonCalc text="/" color="operator" action={btnDivide} />
+            </View>
+
+            {/* Row buttons */}
+            <View style={styles.row}>
+                <ButtonCalc text="7" color="number" action={buildNumber} />
+                <ButtonCalc text="8" color="number" action={buildNumber} />
+                <ButtonCalc text="9" color="number" action={buildNumber} />
+                <ButtonCalc text="X" color="operator" action={btnMultiply} />
+            </View>
+
+            {/* Row buttons */}
+            <View style={styles.row}>
+                <ButtonCalc text="4" color="number" action={buildNumber} />
+                <ButtonCalc text="5" color="number" action={buildNumber} />
+                <ButtonCalc text="6" color="number" action={buildNumber} />
+                <ButtonCalc text="-" color="operator" action={btnSubtract} />
+            </View>
+
+            {/* Row buttons */}
+            <View style={styles.row}>
+                <ButtonCalc text="1" color="number" action={buildNumber} />
+                <ButtonCalc text="2" color="number" action={buildNumber} />
+                <ButtonCalc text="3" color="number" action={buildNumber} />
+                <ButtonCalc text="+" color="operator" action={btnAdd} />
+            </View>
+
+            {/* Row buttons */}
+            <View style={styles.row}>
+                <ButtonCalc text="0" color="number" action={buildNumber} wide />
+                <ButtonCalc text="." color="number" action={buildNumber} />
+                <ButtonCalc text="=" color="operator" action={operation} />
+            </View>
+        </View>
+    )
+}
